f70f60c94fc5b5c9e6a956d170b08100a0bf263e
atif721 <abdullahatif721@outlook.com>
2026-07-05 20:18:12 +0600

    filter, find, indexOf revised

--- Backend Learnings/Lecture_1/script.js
+++ Backend Learnings/Lecture_1/script.js
@@ -13,10 +13,23 @@ var ans1 = arr.map(function (val) {
 });
 console.log(ans1);
 
-// filter 
+// filter uses to filter values in arrays
 var ans2 = arr.filter(function (val) {
   if (val > 3) {
     return true;
   } else return false;
 });
 console.log(ans2);
+
+// find used to find any value either presented in array or not
+var ans3 = arr.find(function (val) {
+  if (val === 3) {
+    return val;
+  }
+});
+
+console.log(ans3);
+
+//indexOf used to find the number of index of an element in array
+var ans4 = arr.indexOf(21);
+console.log(ans4);

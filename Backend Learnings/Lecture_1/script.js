385ac9118f29cabfe219735ac24c1d4b17281717
atif721 <abdullahatif721@outlook.com>
2026-07-05 20:12:37 +0600

    map added

--- Backend Learnings/Lecture_1/script.js
+++ Backend Learnings/Lecture_1/script.js
@@ -2,6 +2,21 @@ var arr = [1, 2, 3, 4];
 
 // foreach,,, map,,, filter,,, find,,, indexOf
 
-arr.forEach(function (val) {
-  console.log(val + " hello");
+// foreach used just to iterate into an array and print the values of the arrays
+// arr.forEach(function (val) {
+//   console.log(val + " hello");
+// });
+
+// map is used to iterate into an array and returns another array
+var ans1 = arr.map(function (val) {
+  return val;
 });
+console.log(ans1);
+
+// filter 
+var ans2 = arr.filter(function (val) {
+  if (val > 3) {
+    return true;
+  } else return false;
+});
+console.log(ans2);

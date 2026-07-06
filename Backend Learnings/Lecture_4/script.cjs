41d8f0b80e2accb90bc3c7afa23e1fcb1f2b8124
atif721 <abdullahatif721@outlook.com>
2026-07-06 13:23:56 +0600

    Lecture-4 finished

--- Backend Learnings/Lecture_4/script.cjs
+++ Backend Learnings/Lecture_4/script.cjs
@@ -1,12 +1,34 @@
 const express = require("express");
 const app = express();
 
+// ------- middleware -------
+app.use((req, res, next) => {
+  console.log(`Middleware 1`);
+  next();
+});
+
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+});
+
+// -------- routes ---------
 app.get("/", (req, res) => {
   res.send("Hello, World!");
 });
 
 app.get("/myprofile", (req, res) => {
   res.send("Hey how are you doing?");
+});
+
+app.get("/myprofile2", (req, res, next) => {
+  return next(new Error("Hey how are you doing?"));
+});
+
+// --------- error handling middleware -----------
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Something went wrong!");
 });
 
 // const PORT = process.env.PORT || 3000;
